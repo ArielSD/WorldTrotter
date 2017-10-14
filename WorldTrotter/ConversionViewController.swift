c2eee536dd34ed8be69699e400ffb99608a9ddd2
Ariel <ariel.scottdicker@shopify.com>
2017-10-14 12:16:21 -0400

    Tap gesture recognizer

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ConversionViewController: UIViewController {
 	@IBOutlet var celsiusLabel: UILabel!
+	@IBOutlet var textField: UITextField!
 	
 	@IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
 		if let text = textField.text, !text.isEmpty {
@@ -17,5 +18,9 @@ class ConversionViewController: UIViewController {
 		} else {
 			celsiusLabel.text = "???"
 		}
+	}
+	
+	@IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+		textField.resignFirstResponder()
 	}
 }
