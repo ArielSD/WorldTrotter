0f36e2f7f9efbf15870dc45458e20b42a5b02ab2
Ariel <ariel.scottdicker@shopify.com>
2017-10-14 12:11:49 -0400

    Editing changed methods on the celsius label

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Ariel Scott-Dicker on 2017-10-14.
+//  Copyright © 2017 Ariel Scott-Dicker. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+	@IBOutlet var celsiusLabel: UILabel!
+	
+	@IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+		if let text = textField.text, !text.isEmpty {
+			celsiusLabel.text = text
+		} else {
+			celsiusLabel.text = "???"
+		}
+	}
+}
