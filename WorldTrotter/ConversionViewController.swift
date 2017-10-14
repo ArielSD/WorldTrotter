be4985da2051cdf54380d87119291d7611a91644
Ariel <ariel.scottdicker@shopify.com>
2017-10-14 12:31:37 -0400

    Temperature conversion

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -12,15 +12,43 @@ class ConversionViewController: UIViewController {
 	@IBOutlet var celsiusLabel: UILabel!
 	@IBOutlet var textField: UITextField!
 	
-	@IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-		if let text = textField.text, !text.isEmpty {
-			celsiusLabel.text = text
+	var fahrenheitValue: Measurement<UnitTemperature>? {
+		didSet {
+			updateCelsiusLabel()
+		}
+	}
+	
+	var celsiusValue: Measurement<UnitTemperature>? {
+		if let fahrenheitValue = fahrenheitValue {
+			return fahrenheitValue.converted(to: .celsius)
 		} else {
-			celsiusLabel.text = "???"
+			return nil
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		updateCelsiusLabel()
+	}
+	
+	@IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+		if let text = textField.text, let value = Double(text) {
+			fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+		} else {
+			fahrenheitValue = nil
 		}
 	}
 	
 	@IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
 		textField.resignFirstResponder()
+	}
+	
+	func updateCelsiusLabel() {
+		if let celsiusValue = celsiusValue {
+			celsiusLabel.text = "\(celsiusValue)"
+		} else {
+			celsiusLabel.text = "???"
+		}
 	}
 }
