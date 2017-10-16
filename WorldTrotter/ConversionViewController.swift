5e3a8d9fae4412324c69317d802334a000724e5c
Ariel <ariel.scottdicker@shopify.com>
2017-10-15 22:01:41 -0400

    Configuring the tab bar

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -37,6 +37,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		print("ConversionViewController loaded its view.")
+		
 		updateCelsiusLabel()
 	}
 	
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Ariel Scott-Dicker on 2017-10-15.
+//  Copyright © 2017 Ariel Scott-Dicker. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		print("MapViewController loaded its view.")
+	}
+}
